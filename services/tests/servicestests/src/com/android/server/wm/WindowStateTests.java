a6c86ceb9d8f06bfecea3e438dddae5a19acc7e7
Brett Chabot <brettchabot@google.com>
2018-07-23 13:08:30 -0700

    Migrate frameworks/base/services/tests/servicestests to androidx.test
    See go/jetpack-test-android-migration
    
    cherrypicked from ag/4608401
    
    Test: atest FrameworksServicesTests
    Change-Id: I81e4ffbd7f345d89b43d10b661dad2f40c270df3

--- services/tests/servicestests/src/com/android/server/wm/WindowStateTests.java
+++ services/tests/servicestests/src/com/android/server/wm/WindowStateTests.java
@@ -21,9 +21,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import android.platform.test.annotations.Presubmit;
-import android.support.test.filters.FlakyTest;
-import android.support.test.filters.SmallTest;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.filters.FlakyTest;
+import androidx.test.filters.SmallTest;
+import androidx.test.runner.AndroidJUnit4;
 
 import java.util.LinkedList;
 
