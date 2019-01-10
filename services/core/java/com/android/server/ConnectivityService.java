63b7891537758ef472af133cc582618a09bb488e
Benedict Wong <benedictwong@google.com>
2019-01-10 15:06:58 -0800

    Fix race condition with network creation and UID ranges
    This commit fixes a race condition where a network is registered with
    UID ranges, and a NetworkRequest is already actively waiting for the
    network. Prior to this change, the UID rules are updated after the
    NetworkRequest callbacks are fired, and leads to a race condition
    between the updating of UIDs and attempts to bind a Socket to the
    Network.
    
    This change moves the UID rule updating until after the network is
    created.
    
    Bug: 120007064
    Test: CTS tests that found the above no longer flaky
    Change-Id: I860b05065b3bb02ee58187d64b3f796369ab9d4d

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -4933,7 +4933,6 @@ public class ConnectivityService extends IConnectivityManager.Stub
         NetworkInfo networkInfo = nai.networkInfo;
         nai.networkInfo = null;
         updateNetworkInfo(nai, networkInfo);
-        updateUids(nai, null, nai.networkCapabilities);
     }
 
     private void updateLinkProperties(NetworkAgentInfo networkAgent, LinkProperties newLp,
@@ -5854,6 +5853,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
                     mNMS.createVirtualNetwork(networkAgent.network.netId,
                             (networkAgent.networkMisc == null ||
                                 !networkAgent.networkMisc.allowBypass));
+                    updateUids(networkAgent, null, networkAgent.networkCapabilities);
                 } else {
                     mNMS.createPhysicalNetwork(networkAgent.network.netId,
                             getNetworkPermission(networkAgent.networkCapabilities));
