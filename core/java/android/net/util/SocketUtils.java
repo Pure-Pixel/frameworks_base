43759306645c90566c05e886f64c2df88e9ef804
Neil Fuller <nfuller@google.com>
2019-06-20 09:02:56 +0100

    Switch to a single PacketSocketAddress constructor
    Switch to a single PacketSocketAddress constructor to
    avoid multiple overloads.
    
    Bug: 133196453
    Bug: 124232146
    Test: build only
    Change-Id: I30533ac3aa99c2cd09e8c624fedf6000b0f52c11

--- core/java/android/net/util/SocketUtils.java
+++ core/java/android/net/util/SocketUtils.java
@@ -69,7 +69,12 @@ public final class SocketUtils {
      */
     @NonNull
     public static SocketAddress makePacketSocketAddress(int protocol, int ifIndex) {
-        return new PacketSocketAddress((short) protocol, ifIndex);
+        return new PacketSocketAddress(
+                (short) protocol /* sll_protocol */,
+                ifIndex /* sll_ifindex */,
+                (short) 0 /* sll_hatype */,
+                (byte) 0 /* sll_pkttyp */,
+                null /* sll_addr */);
     }
 
     /**
@@ -77,7 +82,12 @@ public final class SocketUtils {
      */
     @NonNull
     public static SocketAddress makePacketSocketAddress(int ifIndex, @NonNull byte[] hwAddr) {
-        return new PacketSocketAddress(ifIndex, hwAddr);
+        return new PacketSocketAddress(
+                (short) 0 /* sll_protocol */,
+                ifIndex /* sll_ifindex */,
+                (short) 0 /* sll_hatype */,
+                (byte) 0 /* sll_pkttype */,
+                hwAddr /* sll_addr */);
     }
 
     /**
