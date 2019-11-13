53cfbf9a7aaefe7ef55ebc3067ca36ab522398d7
Benedict Wong <benedictwong@google.com>
2019-11-12 22:31:51 -0800

    Implement Ikev2VpnRunner
    This change adds the implementation for IKEv2/IPsec VPNs.
    
    Bug: 144246767
    Test: Manually tested
    Change-Id: I5ccec756cec49ccf57ccc4d5ad800eeb5d595a76

--- core/java/android/net/Ikev2VpnProfile.java
+++ core/java/android/net/Ikev2VpnProfile.java
@@ -157,6 +157,16 @@ public final class Ikev2VpnProfile extends PlatformVpnProfile {
         VpnProfile.validateAllowedAlgorithms(mAllowedAlgorithms);
     }
 
+    /**
+     * Returns a raw VpnProfile type
+     *
+     * @hide
+     */
+    @NonNull
+    public int getTypeInt() {
+        return mType;
+    }
+
     /** Retrieves the server address string. */
     @NonNull
     public String getServerAddr() {
@@ -315,6 +325,7 @@ public final class Ikev2VpnProfile extends PlatformVpnProfile {
         profile.isMetered = mIsMetered;
         profile.maxMtu = mMaxMtu;
         profile.authParamsInline = true;
+        profile.saveLogin = true;
 
         switch (mType) {
             case TYPE_IKEV2_IPSEC_USER_PASS:
