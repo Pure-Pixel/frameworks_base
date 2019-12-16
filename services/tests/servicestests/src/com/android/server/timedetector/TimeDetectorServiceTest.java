1ec5e4e168b5582b5666690f7d2f0884ab36c6ec
Neil Fuller <nfuller@google.com>
2019-12-16 16:54:06 +0000

    Change NetworkTimeUpdateServiceImpl behavior
    This change moves some logic associated with time detection out of the
    NetworkTimeUpdateServiceImpl and moves it to the TimeDetectorStrategy
    called by the TimeDetectorService.
    
    The TimeDetectorStategy implementation now decides when to use an
    NTP-derived time instead. The logic implemented here is effectively the
    same as it was: NITZ takes priority over NTP until the latest NITZ
    signal becomes "too old".
    
    The motivating reasons for the change:
    1) The "what signal is used when several are available?" logic should be
    in one place: the TimeDetectoryStrategy not scattered around the
    platform. Centralizing will enable different strategies and time sources
    to be introduced over time.
    
    2) It will allow removal of the ACTION_NETWORK_SET_TIME intent in a
    follow-up commit; this intent was used to coordinate the NTP vs NITZ
    logic should no longer be required.
    
    The "bonus" effect of this change is that NetworkTimeUpdateServiceImpl
    becomes more general: It can now be the authority for the NTP time on a
    device independently of how the system clock should be set. e.g. it
    could be used in future to back the
    AlarmManager.currentNetworkTimeMillis() API call.  Anything that needs a
    "what is the best time according to NTP?" answer could use this service.
    
    Note: The NetworkTimeUpdateServiceImpl still observes the AUTO_TIME
    setting and will perform an NTP lookup "on demand" (if required) when
    the user enables the setting. This should be a temporary state and
    likely to be removed in future; knowledge of AUTO_TIME shouldn't be
    required for the NTP info to be correct and if
    NetworkTimeUpdateServiceImpl is doing the right thing.
    
    Bug: 140712361
    Test: atest com.android.server.timedetector
    Test: atest android.app.timedetector
    Change-Id: I4b2936a42c552c4dd4cb55010306bb81bca0c712

--- services/tests/servicestests/src/com/android/server/timedetector/TimeDetectorServiceTest.java
+++ services/tests/servicestests/src/com/android/server/timedetector/TimeDetectorServiceTest.java
@@ -27,6 +27,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import android.app.timedetector.ManualTimeSuggestion;
+import android.app.timedetector.NetworkTimeSuggestion;
 import android.app.timedetector.PhoneTimeSuggestion;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -109,6 +110,36 @@ public class TimeDetectorServiceTest {
         mStubbedTimeDetectorStrategy.verifySuggestManualTimeCalled(manualTimeSuggestion);
     }
 
+    @Test(expected = SecurityException.class)
+    public void testSuggestNetworkTime_withoutPermission() {
+        doThrow(new SecurityException("Mock"))
+                .when(mMockContext).enforceCallingOrSelfPermission(anyString(), any());
+        NetworkTimeSuggestion NetworkTimeSuggestion = createNetworkTimeSuggestion();
+
+        try {
+            mTimeDetectorService.suggestNetworkTime(NetworkTimeSuggestion);
+            fail();
+        } finally {
+            verify(mMockContext).enforceCallingOrSelfPermission(
+                    eq(android.Manifest.permission.SET_TIME), anyString());
+        }
+    }
+
+    @Test
+    public void testSuggestNetworkTime() throws Exception {
+        doNothing().when(mMockContext).enforceCallingOrSelfPermission(anyString(), any());
+
+        NetworkTimeSuggestion NetworkTimeSuggestion = createNetworkTimeSuggestion();
+        mTimeDetectorService.suggestNetworkTime(NetworkTimeSuggestion);
+        mTestHandler.assertTotalMessagesEnqueued(1);
+
+        verify(mMockContext).enforceCallingOrSelfPermission(
+                eq(android.Manifest.permission.SET_TIME), anyString());
+
+        mTestHandler.waitForEmptyQueue();
+        mStubbedTimeDetectorStrategy.verifySuggestNetworkTimeCalled(NetworkTimeSuggestion);
+    }
+
     @Test
     public void testDump() {
         when(mMockContext.checkCallingOrSelfPermission(android.Manifest.permission.DUMP))
@@ -146,11 +177,17 @@ public class TimeDetectorServiceTest {
         return new ManualTimeSuggestion(timeValue);
     }
 
+    private static NetworkTimeSuggestion createNetworkTimeSuggestion() {
+        TimestampedValue<Long> timeValue = new TimestampedValue<>(100L, 1_000_000L);
+        return new NetworkTimeSuggestion(timeValue);
+    }
+
     private static class StubbedTimeDetectorStrategy implements TimeDetectorStrategy {
 
         // Call tracking.
         private PhoneTimeSuggestion mLastPhoneSuggestion;
         private ManualTimeSuggestion mLastManualSuggestion;
+        private NetworkTimeSuggestion mLastNetworkSuggestion;
         private boolean mLastAutoTimeDetectionToggleCalled;
         private boolean mDumpCalled;
 
@@ -171,6 +208,12 @@ public class TimeDetectorServiceTest {
         }
 
         @Override
+        public void suggestNetworkTime(NetworkTimeSuggestion timeSuggestion) {
+            resetCallTracking();
+            mLastNetworkSuggestion = timeSuggestion;
+        }
+
+        @Override
         public void handleAutoTimeDetectionChanged() {
             resetCallTracking();
             mLastAutoTimeDetectionToggleCalled = true;
@@ -185,6 +228,7 @@ public class TimeDetectorServiceTest {
         void resetCallTracking() {
             mLastPhoneSuggestion = null;
             mLastManualSuggestion = null;
+            mLastNetworkSuggestion = null;
             mLastAutoTimeDetectionToggleCalled = false;
             mDumpCalled = false;
         }
@@ -195,6 +239,10 @@ public class TimeDetectorServiceTest {
 
         public void verifySuggestManualTimeCalled(ManualTimeSuggestion expectedSuggestion) {
             assertEquals(expectedSuggestion, mLastManualSuggestion);
+        }
+
+        public void verifySuggestNetworkTimeCalled(NetworkTimeSuggestion expectedSuggestion) {
+            assertEquals(expectedSuggestion, mLastNetworkSuggestion);
         }
 
         void verifyHandleAutoTimeDetectionToggleCalled() {
