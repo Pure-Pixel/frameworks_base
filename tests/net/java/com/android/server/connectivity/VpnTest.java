10d45e798b5398704b396b29231dbc555e7cc6d2
Benedict Wong <benedictwong@google.com>
2020-01-10 16:02:18 -0800

    Rename VpnConfig.LEGACY_VPN to SETTING_VPN
    This change renames the pseudo-package used for settings-based VPNs to
    encompass Platform VPNs that are configured from the Settings VPNs menu.
    
    App-based platform VPNs will use the package name of the app as a key,
    while the settings-based ones will use the pseudo-package.
    
    Once the Legacy VPN is removed (not just deprecated), the Settings app
    should use the standard VpnManger APIs (with one or two tweaks to allow
    for multiple profiles per app)
    
    Bug: 146577177
    Test: FrameworksNetTests passing
    Change-Id: Id8039ffc6363959fcdb40a52548af23bd15dca02

--- tests/net/java/com/android/server/connectivity/VpnTest.java
+++ tests/net/java/com/android/server/connectivity/VpnTest.java
@@ -476,7 +476,7 @@ public class VpnTest {
         order.verify(mNetService).setAllowOnlyVpnForUids(eq(true), aryEq(exceptPkg0));
 
         // When that VPN package is unset, everything should be undone again in reverse.
-        vpn.prepare(null, VpnConfig.LEGACY_VPN);
+        vpn.prepare(null, VpnConfig.SETTINGS_VPN);
         order.verify(mNetService).setAllowOnlyVpnForUids(eq(false), aryEq(exceptPkg0));
         order.verify(mNetService).setAllowOnlyVpnForUids(eq(true), aryEq(entireUser));
     }
