cee1ba8c3a2e74bd2a80bc5ae4466d7f6e57be42
Benedict Wong <benedictwong@google.com>
2020-10-01 17:49:42 -0700

    Implement Disconnected state
    This change implements the signal handling for the Disconnected state.
    
    Bug: 165827287
    Test: atest FrameworksVcnTests
    Change-Id: I4853b0801b82fddc4ef9e9c6e6a659f9b81e2ac3

--- tests/vcn/java/com/android/server/vcn/VcnGatewayConnectionTest.java
+++ tests/vcn/java/com/android/server/vcn/VcnGatewayConnectionTest.java
@@ -17,15 +17,9 @@
 package com.android.server.vcn;
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
 
-import android.annotation.NonNull;
-import android.content.Context;
 import android.net.NetworkCapabilities;
 import android.net.vcn.VcnGatewayConnectionConfigTest;
-import android.os.ParcelUuid;
-import android.os.test.TestLooper;
-import android.telephony.SubscriptionInfo;
 
 import androidx.test.filters.SmallTest;
 import androidx.test.runner.AndroidJUnit4;
@@ -33,42 +27,10 @@ import androidx.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
 /** Tests for TelephonySubscriptionTracker */
 @RunWith(AndroidJUnit4.class)
 @SmallTest
-public class VcnGatewayConnectionTest {
-    private static final ParcelUuid TEST_PARCEL_UUID = new ParcelUuid(UUID.randomUUID());
-    private static final int TEST_SIM_SLOT_INDEX = 1;
-    private static final int TEST_SUBSCRIPTION_ID_1 = 2;
-    private static final SubscriptionInfo TEST_SUBINFO_1 = mock(SubscriptionInfo.class);
-    private static final int TEST_SUBSCRIPTION_ID_2 = 3;
-    private static final SubscriptionInfo TEST_SUBINFO_2 = mock(SubscriptionInfo.class);
-    private static final Map<Integer, ParcelUuid> TEST_SUBID_TO_GROUP_MAP;
-
-    static {
-        final Map<Integer, ParcelUuid> subIdToGroupMap = new HashMap<>();
-        subIdToGroupMap.put(TEST_SUBSCRIPTION_ID_1, TEST_PARCEL_UUID);
-        subIdToGroupMap.put(TEST_SUBSCRIPTION_ID_2, TEST_PARCEL_UUID);
-        TEST_SUBID_TO_GROUP_MAP = Collections.unmodifiableMap(subIdToGroupMap);
-    }
-
-    @NonNull private final Context mContext;
-    @NonNull private final TestLooper mTestLooper;
-    @NonNull private final VcnNetworkProvider mVcnNetworkProvider;
-    @NonNull private final VcnGatewayConnection.Dependencies mDeps;
-
-    public VcnGatewayConnectionTest() {
-        mContext = mock(Context.class);
-        mTestLooper = new TestLooper();
-        mVcnNetworkProvider = mock(VcnNetworkProvider.class);
-        mDeps = mock(VcnGatewayConnection.Dependencies.class);
-    }
-
+public class VcnGatewayConnectionTest extends VcnGatewayConnectionTestBase {
     @Test
     public void testBuildNetworkCapabilities() throws Exception {
         final NetworkCapabilities caps =
