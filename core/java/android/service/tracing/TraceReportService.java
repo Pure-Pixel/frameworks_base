8abe36b8b1a0c68d99a67f2f0709fd1ab48442b2
Lalit Maganti <lalitm@google.com>
2021-12-10 21:19:56 +0000

    base: add support for routing traces to reporters
    Change-Id: I4ff55e9c211bfedada85234ee86f7484acf633ee

---
+++ core/java/android/service/tracing/TraceReportService.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.service.tracing;
+
+import static android.annotation.SystemApi.Client.PRIVILEGED_APPS;
+
+import android.annotation.NonNull;
+import android.annotation.Nullable;
+import android.annotation.SystemApi;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.ParcelFileDescriptor;
+import android.os.Process;
+import android.tracing.TraceReportParams;
+import android.util.Log;
+
+import java.io.IOException;
+
+/** @hide */
+@SystemApi(client = PRIVILEGED_APPS)
+public class TraceReportService extends Service {
+    private static final String TAG = "TraceReportService";
+    private Messenger mMessenger = null;
+
+    /** @hide */
+    public static final int MSG_REPORT_TRACE = 1;
+
+    /** @hide */
+    @SystemApi(client = PRIVILEGED_APPS)
+    public static final class TraceParams {
+        private final ParcelFileDescriptor mFd;
+        private final long mUuidLsb;
+        private final long mUuidMsb;
+
+        /** @hide */
+        public TraceParams(TraceReportParams params) {
+            mFd = params.fd;
+            mUuidLsb = params.uuidLsb;
+            mUuidMsb = params.uuidMsb;
+        }
+
+        @NonNull
+        public ParcelFileDescriptor getFd() {
+            return mFd;
+        }
+
+        public long getUuidLsb() {
+            return mUuidLsb;
+        }
+
+        public long getUuidMsb() {
+            return mUuidMsb;
+        }
+    }
+
+    // Methods to override.
+    /**
+     * Called when a trace is reported and sent to this class.
+     *
+     * Note: the trace file descriptor should not be persisted beyond the lifetime of this
+     * function as it will be closed immediately after this function returns: if future use of
+     * the fd is needed, it should be duped.
+     */
+    public void onReportTrace(@NonNull TraceParams args) {
+    }
+
+    // Optional methods to override.
+    // Realistically, these methods are internal implementation details but since this class is
+    // a SystemApi, it's better to err on the side of flexibility just in-case we need to override
+    // these methods down the line.
+    public boolean onMessage(@NonNull Message msg) {
+        if (msg.what == MSG_REPORT_TRACE) {
+            if (msg.sendingUid != Process.SYSTEM_UID) {
+                Log.e(TAG, "Invalid UID " + msg.sendingUid + " for report trace message.");
+                return false;
+            }
+            if (!(msg.obj instanceof TraceReportParams)) {
+                Log.e(TAG, "Received invalid type for report trace message.");
+                return false;
+            }
+            TraceParams params = new TraceParams((TraceReportParams) msg.obj);
+            try {
+                onReportTrace(params);
+            } finally {
+                onCloseTraceFd(params.getFd());
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Closes the trace file descriptor. This is called immediately after onReportTrace.
+     */
+    public void onCloseTraceFd(@NonNull ParcelFileDescriptor fd) {
+        try {
+            fd.close();
+        } catch (IOException ignored) {
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(@NonNull Intent intent) {
+        if (mMessenger == null) {
+            mMessenger = new Messenger(new Handler(Looper.getMainLooper(), this::onMessage));
+        }
+        return mMessenger.getBinder();
+    }
+}
