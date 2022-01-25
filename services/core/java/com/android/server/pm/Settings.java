6d9b7dd91376d8f518668999367c944ba5c5abe0
Kevin Jeon <kevinjeon@google.com>
2022-01-25 22:04:30 +0000

    Annotate dump methods with @NeverCompile [4/5]
    This change annotates dump/debug methods in services.odex that are
    larger than 10000 bytes with NeverCompile so that they will not be
    compiled. This results in an overall ~1% decrease in the size of
    services.odex.
    
    Bug: 215417388
    Test: Build and check that these methods are not called during boot or
          app startup.
    Change-Id: I07a13fbd72c84229ac42c3779abc352ba3f8086c
    Merged-In: I07a13fbd72c84229ac42c3779abc352ba3f8086c

--- services/core/java/com/android/server/pm/Settings.java
+++ services/core/java/com/android/server/pm/Settings.java
@@ -129,6 +129,8 @@ import com.android.server.utils.WatchedSparseArray;
 import com.android.server.utils.WatchedSparseIntArray;
 import com.android.server.utils.Watcher;
 
+import dalvik.annotation.optimization.NeverCompile;
+
 import libcore.io.IoUtils;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -4372,6 +4374,7 @@ public final class Settings implements Watchable, Snappable {
         pw.decreaseIndent();
     }
 
+    @NeverCompile // Avoid size overhead of debugging code.
     void dumpPackageLPr(PrintWriter pw, String prefix, String checkinTag,
             ArraySet<String> permissionNames, PackageSetting ps,
             LegacyPermissionState permissionsState, SimpleDateFormat sdf, Date date,
