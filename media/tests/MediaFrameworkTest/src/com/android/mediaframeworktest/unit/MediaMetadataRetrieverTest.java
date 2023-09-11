b55c26ed9f57aa4a341c71c222e739b604a0999e
Venkatarama Avadhani <venkatarama.avadhani@ittiam.com>
2023-09-11 08:47:33 +0530

    Test Flag for Default Thumbnail Colorspace
    Add tests to verify thumbnail generation with and without the flag being
    set to use RGB8888 colorspace as default.
    
    Test: atest testRGB8888DefaultThumbnail
    Bug: 298965955
    
    Change-Id: I8452e75e07bcfcd2483ad2da971806147f5a9067

--- media/tests/MediaFrameworkTest/src/com/android/mediaframeworktest/unit/MediaMetadataRetrieverTest.java
+++ media/tests/MediaFrameworkTest/src/com/android/mediaframeworktest/unit/MediaMetadataRetrieverTest.java
@@ -18,13 +18,20 @@ package com.android.mediaframeworktest.unit;
 
 import android.graphics.Bitmap;
 import android.media.MediaMetadataRetriever;
+import android.platform.test.annotations.RequiresFlagsDisabled;
+import android.platform.test.annotations.RequiresFlagsEnabled;
+import android.platform.test.flag.junit.CheckFlagsRule;
+import android.platform.test.flag.junit.DeviceFlagsValueProvider;
 import android.test.AndroidTestCase;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.util.Log;
 
+import com.android.media.playback.flags.Flags;
 import com.android.mediaframeworktest.MediaNames;
 import com.android.mediaframeworktest.MediaProfileReader;
+
+import org.junit.Rule;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -270,6 +277,49 @@ public class MediaMetadataRetrieverTest extends AndroidTestCase {
         retriever.release();
         assertTrue(!hasFailed);
     }
+
+
+    @Rule
+    public final CheckFlagsRule mCheckFlagsRule = DeviceFlagsValueProvider.createCheckFlagsRule();
+
+    /** Test the thumbnail is generated when the default is set to ARGB8888 */
+    @MediumTest
+    @RequiresFlagsDisabled(Flags.FLAG_MEDIAMETADATARETRIEVER_DEFAULT_RGB8888)
+    public void testRGB565DefaultThumbnail() throws IOException {
+        boolean hasFailed = false;
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        try {
+            retriever.setDataSource(MediaNames.TEST_PATH_1);
+            Bitmap bitmap = retriever.getFrameAtTime(-1);
+            assertTrue(bitmap != null);
+            Log.e(TAG, "The bitmap colorspace is " + bitmap.getColorSpace());
+        } catch (Exception e) {
+            Log.e(TAG, "Fails to setDataSource for " + MediaNames.TEST_PATH_1, e);
+            hasFailed = true;
+        }
+        retriever.release();
+        assertFalse(hasFailed);
+    }
+
+    /** Test the thumbnail is generated when the default is not set to ARGB8888 */
+    @MediumTest
+    @RequiresFlagsEnabled(Flags.FLAG_MEDIAMETADATARETRIEVER_DEFAULT_RGB8888)
+    public void testRGB8888DefaultThumbnail() throws IOException {
+        boolean hasFailed = false;
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        try {
+            retriever.setDataSource(MediaNames.TEST_PATH_1);
+            Bitmap bitmap = retriever.getFrameAtTime(-1);
+            assertTrue(bitmap != null);
+            Log.e(TAG, "The bitmap colorspace is " + bitmap.getColorSpace());
+        } catch (Exception e) {
+            Log.e(TAG, "Fails to setDataSource for " + MediaNames.TEST_PATH_1, e);
+            hasFailed = true;
+        }
+        retriever.release();
+        assertFalse(hasFailed);
+    }
+
 
     // TODO:
     // Encode and test for the correct mix of metadata elements on a per-file basis?
